80ee54b961d7f1bcad8a7bdf3b528054c6de614d
Eduard Malakhov <eduard@stackeat.com>
2019-08-13 17:34:36 +0700

    Add method arguments in ConfigurationSyntax

--- packages/main/src/conventions/BundlingSyntax.ts
+++ packages/main/src/conventions/BundlingSyntax.ts
@@ -1,11 +1,11 @@
 import { PurelyFunctional } from '@stackeat/primitives';
 import { ExtensionModule } from 'conventions/ExtensionModule';
 
-type ConfigurationSyntax<T> = {
-    [key in keyof T]: () => ConfigurationSyntax<T>;
+type ConfigurationSyntax<T extends PurelyFunctional<T>> = {
+    [K in keyof T]: (...args: Parameters<T[K]>) => ConfigurationSyntax<T>;
 };
 
-type ConfigurationRoutine<T> = (configuration: ConfigurationSyntax<T>) => void;
+type ConfigurationRoutine<T extends PurelyFunctional<T>> = (configuration: ConfigurationSyntax<T>) => void;
 
 interface BundlingSyntax<TEA extends PurelyFunctional<TEA>, TCA extends PurelyFunctional<TCA>> {
     bundle<TECA>(extension: ExtensionModule<TEA, TECA>): BundlingSyntax<TEA, TCA & TECA>;
